2336ba29c19b1a7e2098679aacd7dc92fe83df90
Vijayaragavan <ragavvj77@gmail.com>
2024-10-18 18:26:52 +0530

    Update1 Test.java

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ public class Test {
         int a=5;
         int b=10;
         int c=6;
-        int res=a+b-c;
+        int res=a+b+c;
         System.out.println("the result is " +res);
     }
     public static void main(String[] args)
